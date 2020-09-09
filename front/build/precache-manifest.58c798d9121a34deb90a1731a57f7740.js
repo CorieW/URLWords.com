136e911f3985349644216f48e48019d62b70eb25
CorieW <watson.corie@gmail.com>
2020-09-09 07:26:36 +0100

    Added build.
    Current error with not using backend server app.

---
+++ front/build/precache-manifest.58c798d9121a34deb90a1731a57f7740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cedc667dc2e30eec2ef4d912f650b77f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e91f2636424df4d277a",
+    "url": "/static/css/main.39feb244.chunk.css"
+  },
+  {
+    "revision": "3fc05764cbfdd42b6a7d",
+    "url": "/static/js/2.8c8c8338.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8c8c8338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e91f2636424df4d277a",
+    "url": "/static/js/main.e085cc46.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  }
+]);
